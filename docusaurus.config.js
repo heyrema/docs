34ac91b3196937d183cef2c6e35294c8c0054c25
Param Siddharth <contact@paramsid.com>
2022-03-06 23:16:19 +0530

    Fix editing (resolves paramsiddharth/rema#58)
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,13 +25,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/paramsiddharth/rema-docs/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/paramsiddharth/rema-docs/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/paramsiddharth/rema-docs/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/paramsiddharth/rema-docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
